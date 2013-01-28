5b8449d05e3d3ae8475316d8cbae1dab31b0e11f
Cameron Mullen <cameron.mullen@gmail.com>
2013-01-28 15:18:52 -0500

    Added comments to LoyaltyPersistence interface Implemented TempLoyaltyPersistence

--- CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/dao/LoyaltyPersistence.java
+++ CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/dao/LoyaltyPersistence.java
@@ -10,22 +10,72 @@ import com.mullen.CoalitionLoyalty.model.User;
 
 /**
  * @author cmullen
- *
+ * 
  */
 public interface LoyaltyPersistence {
-	
+
+	/**
+	 *  RESULT CODE: Operation was successful
+	 */
 	public static final int SUCCESS = 1;
+	
+	/**
+	 * RESULT CODE: Operation had an unknown error
+	 */
 	public static final int UNKNOWN_ERROR = 0;
+
 	
+	/**
+	 * Gets all Location objects in storage
+	 * @return A List of all Location objects in storage. Returns an empty list if no Locations stored.
+	 */
+	public List<Location> getAllLocations();
+
+	/**
+	 * Gets all Users in storage
+	 * @return A List of all Users in storage. Returns an empty list if no Users stored. 
+	 */
+	public List<User> getAllUsers();
 	
-	public User getUser(int userID);
-	public int updateUser(User usr);
+	/**
+	 * Retrieves a User from persistent storage given its ID
+	 * 
+	 * @param locationID
+	 *            The ID of the Location
+	 * @return A User object representing the Location in persistent storage.
+	 *         Returns null if no User found.
+	 */
 	
 	public Location getLocation(int locationID);
+
+	
+	
+	/**
+	 * Retrieves a User from persistent storage given its ID
+	 * 
+	 * @param userID
+	 *            The ID of the User
+	 * @return A User object representing the User in persistent storage.
+	 *         Returns null if no User found.
+	 */
+	public User getUser(int userID);
+
+	
+	/**
+	 * Updates the Location in persistent storage. If it does not exist, the Location is added.
+	 * @param loc The Location object to be updated.
+	 * @return A RESULT CODE based upon the outcome of the operation
+	 */
+	
 	public int updateLocation(Location loc);
+
 	
-	public List<User> getAllUsers();
-	public List<Location> getAllLocations();
+	/**
+	 * Updates the User in persistent storage. If it does not exist, the Location is added.
+	 * @param usr The User object to be updated.
+	 * @return A RESULT CODE based upon the outcome of the operation
+	 */
 	
+	public int updateUser(User usr);
 
 }
---
+++ CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/dao/TempLoyaltyPersistence.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.mullen.CoalitionLoyalty.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.mullen.CoalitionLoyalty.model.Location;
+import com.mullen.CoalitionLoyalty.model.User;
+
+/**
+ * @author cmullen
+ *
+ */
+public class TempLoyaltyPersistence implements LoyaltyPersistence {
+
+	Map<Integer, User> allUsers = new HashMap<Integer, User>();
+	Map<Integer, Location> allLocations = new HashMap<Integer, Location>();
+	
+	
+	/* (non-Javadoc)
+	 * @see com.mullen.CoalitionLoyalty.dao.LoyaltyPersistence#getUser(int)
+	 */
+	@Override
+	public User getUser(int userID) {
+		return allUsers.get(userID);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.mullen.CoalitionLoyalty.dao.LoyaltyPersistence#updateUser(com.mullen.CoalitionLoyalty.model.User)
+	 */
+	@Override
+	public int updateUser(User usr) {
+		allUsers.put(usr.getUserID(), usr);
+		return SUCCESS;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.mullen.CoalitionLoyalty.dao.LoyaltyPersistence#getLocation(int)
+	 */
+	@Override
+	public Location getLocation(int locationID) {
+		return allLocations.get(locationID);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.mullen.CoalitionLoyalty.dao.LoyaltyPersistence#updateLocation(com.mullen.CoalitionLoyalty.model.Location)
+	 */
+	@Override
+	public int updateLocation(Location loc) {
+		allLocations.put(loc.getLocationID(), loc);
+		return SUCCESS;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.mullen.CoalitionLoyalty.dao.LoyaltyPersistence#getAllUsers()
+	 */
+	@Override
+	public List<User> getAllUsers() {
+		return new ArrayList<User>(allUsers.values());
+	}
+
+	/* (non-Javadoc)
+	 * @see com.mullen.CoalitionLoyalty.dao.LoyaltyPersistence#getAllLocations()
+	 */
+	@Override
+	public List<Location> getAllLocations() {
+		return new ArrayList<Location>(allLocations.values());
+	}
+
+}
