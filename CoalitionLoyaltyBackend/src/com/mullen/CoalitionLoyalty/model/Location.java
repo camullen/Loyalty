7d16a12ca3c99d5571faf93e3d66ea168762029d
Cameron Mullen <cameron.mullen@gmail.com>
2013-01-28 14:30:05 -0500

    Changed Loyalty Brain

--- CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/model/Location.java
+++ CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/model/Location.java
@@ -1,10 +1,39 @@
 package com.mullen.CoalitionLoyalty.model;
 
+import java.util.Random;
+
 public class Location {
+	public static Random RAND_GEN = new Random();
+	public static final int DEFAULT_VISIT_VALUE = 1;
+	public static final int DEFAULT_REDEEM_COST = 10;
+	
+	
 	//FIELDS
 	private int locationID;
 	private int visitValue;
 	private int redeemCost;
+	
+	
+	
+	//Constructors
+	
+	public Location(){
+		this(DEFAULT_VISIT_VALUE, DEFAULT_REDEEM_COST);
+	}
+	
+	public Location(int visitValue, int redeemCost){
+		this(RAND_GEN.nextInt(), visitValue, redeemCost);
+	}
+	
+	public Location(int locationID, int visitValue, int redeemCost){
+		this.locationID = locationID;
+		this.visitValue = visitValue;
+		this.redeemCost = redeemCost;
+	}
+	
+	
+	
+	
 	
 	//GETTERS & SETTERS
 	
--- CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/model/LoyaltyBrain.java
+++ CoalitionLoyaltyBackend/src/com/mullen/CoalitionLoyalty/model/LoyaltyBrain.java
@@ -2,10 +2,25 @@ package com.mullen.CoalitionLoyalty.model;
 
 public class LoyaltyBrain {
 
-	public boolean doVisit(User usr, Location loc){
+	public static final int SUCCESS = 1;
+	public static final int UNKNOWN_ERROR = 0;
+	public static final int INSUFFICIENT_POINTS = -1;
+	
+	public int doVisit(User usr, Location loc){
+		usr.incrementPoints(loc.getVisitValue());
 		
-		
-		return false;
+		return SUCCESS;
 	}
+	
+	public int doRedeem(User usr, Location loc){
+		boolean hasEnough = usr.decreasePoints(loc.getRedeemCost());
+		if(hasEnough)
+			return SUCCESS;
+		else
+			return INSUFFICIENT_POINTS;
+		
+	}
+	
+	
 	
 }
